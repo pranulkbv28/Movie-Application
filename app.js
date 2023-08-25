9307628654b1e383bacb74a179ae4fa616de7d34
pranulkbv28 <28bvpranaav@gmail.com>
2023-08-26 01:17:44 +0530

    working json file

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-let api = "https://www.omdbapi.com/?&apikey=b938e68c&t=rrr";
+let api = "https://www.omdbapi.com/?&apikey=b938e68c&t=avengers";
+
+fetch(api).then((data)=>{
+    // console.log(data);
+    return data.json()
+}).then((data)=>{
+    console.log(data);
+})
